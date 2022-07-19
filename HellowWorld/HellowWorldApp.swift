5a1c44c3d5f5c69552711e6cf4cd9525c6243169
Todd James <todd@itodd.co>
2022-07-19 17:13:52 -0400

    Initial Commit

---
+++ HellowWorld/HellowWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HellowWorldApp.swift
+//  HellowWorld
+//
+//  Created by Todd James on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct HellowWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
